3e67c70072099e767c139ac4a0201c82f9b9b32b
J-Ahmed-Gaafar <ahmed.fhesham@jumia.com>
2025-04-21 22:28:06 +0200

    17. Service container & services - part 1

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Services\GiftsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,14 +10,24 @@ use App\Entity\User;
 
 class DefaultController extends AbstractController
 {
+
+    public function __construct(GiftsService $gifts)
+    {
+        $this->gifts = ['a', 'b', 'c', 'd'];
+    }
+
     /**
      * @Route("/", name="default")
      */
-    public function index()
+    public function index(GiftsService  $gifts)
     {
 //        $users = ['Adam', 'Robert', 'John', 'Susan'];
 
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+//        $gifts = ['flowers', 'car', 'piano', 'money'];
+//        shuffle($gifts);
+
 //        $entityManager = $this->getDoctrine()->getManager();
 
 //        $user = new User;
@@ -38,6 +49,7 @@ class DefaultController extends AbstractController
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'users' => $users,
+            'random_gift' => $gifts->gifts,
         ]);
 
 //        return $this->render('default/index.html.twig', [
