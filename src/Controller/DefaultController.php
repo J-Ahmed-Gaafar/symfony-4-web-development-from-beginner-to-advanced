03eb8ba065aee06de53c9665e3b2521eb2c8bf56
J-Ahmed-Gaafar <ahmed.fhesham@jumia.com>
2025-04-21 21:52:13 +0200

    16. Entities (models)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,15 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default/{name}", name="default")
+     * @Route("/", name="default")
      */
-    public function index($name)
+    public function index()
     {
-        $users = ['Adam', 'Robert', 'John', 'Susan'];
+//        $users = ['Adam', 'Robert', 'John', 'Susan'];
+
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+//        $entityManager = $this->getDoctrine()->getManager();
+
+//        $user = new User;
+//        $user->setName('Adam');
+//        $user2 = new User;
+//        $user2->setName('Robert');
+//        $user3 = new User;
+//        $user3->setName('John');
+//        $user4 = new User;
+//        $user4->setName('Susan');
+//
+//        $entityManager->persist($user);
+//        $entityManager->persist($user2);
+//        $entityManager->persist($user3);
+//        $entityManager->persist($user4);
+//
+//        $entityManager->flush();
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
